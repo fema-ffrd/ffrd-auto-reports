fd3675749f443fc572f7956168d7b7cea908b159
zherbz <zherbz32@gmail.com>
2024-08-07 19:20:23 +0000

    updated page layout

--- src/Auto-Report.py
+++ src/Auto-Report.py
@@ -10,9 +10,15 @@ import streamlit as st
 from docx import Document
 from pathlib import Path
 import warnings
+import streamlit.components.v1 as components
+
 warnings.filterwarnings("ignore")
 
-from app_utilities import compress_directory, write_session_parameters, initialize_session
+from app_utilities import (
+    compress_directory,
+    write_session_parameters,
+    initialize_session,
+)
 
 # Determine where the script is located
 currDir = os.path.dirname(os.path.realpath(__file__))
@@ -26,45 +32,251 @@ sys.path.append(srcDir)
 
 from auto_report import main_auto_report
 
-# Functions ###################################################################
+particles_js = """<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Particles.js</title>
+  <style>
+  #particles-js {
+    position: fixed;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    z-index: -1; /* Send the animation to the back */
+  }
+  .content {
+    position: relative;
+    z-index: 1;
+    color: white;
+  }
+  
+</style>
+</head>
+<body>
+  <div id="particles-js"></div>
+  <div class="content">
+    <!-- Placeholder for Streamlit content -->
+  </div>
+  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+  <script>
+    particlesJS("particles-js", {
+      "particles": {
+        "number": {
+          "value": 500,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#0000ff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#ffffff"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 0.8,
+          "random": false,
+          "anim": {
+            "enable": false,
+            "speed": 1,
+            "opacity_min": 0.2,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 2,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 50,
+            "size_min": 0.1,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": true,
+          "distance": 100,
+          "color": "#0000ff",
+          "opacity": 0.5,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 0.2,
+          "direction": "none",
+          "random": false,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": true,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "grab"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 100,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 400,
+            "size": 2,
+            "duration": 2,
+            "opacity": 0.5,
+            "speed": 1
+          },
+          "repulse": {
+            "distance": 200,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 2
+          },
+          "remove": {
+            "particles_nb": 3
+          }
+        }
+      },
+      "retina_detect": true
+    });
+  </script>
+</body>
+</html>
+"""
+
+# Main Page ###################################################################
+
+st.set_page_config(layout="wide", page_icon=":globe_with_meridians:")
 
 if __name__ == "__main__":
-
     # setup a session data when app is first opened or browser reset
     if "session_id" not in st.session_state:
-        initialize_session(dataDir)
+        initialize_session(rootDir)
 
-    session_id = st.session_state["session_id"].strftime("%Y_%b_%d_%H_%M_%S_%f")
+    st.title(
+        "Welcome to the Automated Report Generator for FFRD hydraulic HEC-RAS models! 👋"
+    )
 
-    st.set_page_config(layout="wide", page_icon=":globe_with_meridians:")
+    session_data_dir = st.session_state["session_data_dir"]
+    col1, col2, col3 = st.columns(3)
 
-    # Title
-    st.title("Welcome to the Automated Report Generator for FFRD hydraulic HEC-RAS models! 👋")
-    st.write("""
-             This tool is intended to automate the process of generating reports for hydraulic FFRD models created in HEC-RAS.
-             
-             Please reach out to us if you have any questions or need assistance with your projects.""")
-    
-    # Begin User Input
-    st.subheader("Required:")
-    st.write("file paths from the developed HEC-RAS model")
-    GEOM_HDF_PATH = st.text_input("Geometry HDF File", "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.g06.hdf")
-    PLAN_HDF_PATH = st.text_input("Plan HDF File", "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.p01.hdf")
-    REPORT_FILE = st.file_uploader("Report Document", type=["docx"], accept_multiple_files=False)
+    # Intro, and required inputs
+    with col1:
+        st.subheader("Introduction:")
+        st.write(
+            """
+              This tool is intended to automate the process of generating reports for hydraulic FFRD models created in HEC-RAS.
+              
+              Model geometry and plan HDF files may be uploaded locally or acquired from S3 with a provided URI. Afterwards, a multitude
+               of datasets and analyses are processed to generate a comprehensive report as a Word document.
+              The following steps are outlined for this tool to processes as it generates the report:"""
+        )
+        st.markdown(
+            """
+                  1. Processing for the HDF geometry data
+                  2. Processing for the HDF plan data
+                  3. Processing for USGS gage metadata
+                  4. Processing for the HUC04 pilot boundary
+                  5. Processing for the DEM dataset
+                  6. Processing for the NHD stream network
+                  7. Processing for the streamflow period of record
+                  8. Processing for the NLCD data
+                  9. Processing for the constructed model mesh
+                  10. Processing for the max WSE errors
+                  11. Processing for the WSE time to peak
+                  12. Processing for the calibration hydrographs
+                  """
+        )
 
-    st.subheader("Optional:")
-    st.write("the name of the 2D flow area within the HEC-RAS model. Only necessary if more than one 2D flow area is present.")
-    DOMAIN_ID = st.text_input("Domain ID", None)
-    st.write("filter to main streams that occur X times or more within the NHDPlus HR network")
-    STREAM_THRESHOLD = st.number_input("Stream Threshold", 20)
-    st.write("Spatial resolution of the NLCD data")
-    NLCD_RES = st.number_input("NLCD Resolution", 30)
-    st.write("NLCD Year")
-    NLCD_YR = st.number_input("NLCD Year", 2019)
-    st.write("Threshold for the histogram's x-axis for the water surface elevation error")
-    WSE_ERROR_THRESHOLD = st.number_input("WSE Error Threshold", 0.2)
-    st.write("Number of bins for the histogram to plot with respect to cells within the model")
-    NUM_BINS = st.number_input("Number of Bins", 100)
+        st.subheader("Required Input:")
+        st.write("File paths from the developed HEC-RAS model")
+        selected_file_source = st.radio("Select the file source:", ("Local", "S3"))
+        if selected_file_source == "Local":
+            GEOM_HDF_PATH = st.file_uploader(
+                "Geometry HDF File", type=["hdf"], accept_multiple_files=False
+            )
+            PLAN_HDF_PATH = st.file_uploader(
+                "Plan HDF File", type=["hdf"], accept_multiple_files=False
+            )
+        else:
+            GEOM_HDF_PATH = st.text_input(
+                "Geometry HDF File",
+                "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.g06.hdf",
+            )
+            PLAN_HDF_PATH = st.text_input(
+                "Plan HDF File",
+                "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.p01.hdf",
+            )
+
+    # Particles.js animation
+    with col2:
+        if "session_id" in st.session_state:
+            components.html(particles_js, scrolling=False, height=1000, width=500)
+
+    # Optional inputs
+    with col3:
+        st.subheader("Optional Input:")
+        st.write(
+            "The name of the 2D flow area within the HEC-RAS model. Only necessary if more than one 2D flow area is present."
+        )
+        DOMAIN_ID = st.text_input("Domain ID", None)
+        st.write(
+            "Filter to main streams that occur X times or more within the NHDPlus HR network"
+        )
+        STREAM_THRESHOLD = st.number_input("Stream Threshold", 20)
+        st.write("National Inventory of Dams (NID) vertical height criteria")
+        NID_DAM_HEIGHT = st.number_input("Height (ft)", 30)
+        st.write("Spatial resolution of the NLCD data")
+        NLCD_RES = st.number_input("NLCD Resolution", 30)
+        st.write("National Land Cover Database year of coverage")
+        NLCD_YR = st.number_input("NLCD Year", 2019)
+        st.write(
+            "Threshold of the histogram's x-axis for plotting the water surface elevation errors"
+        )
+        WSE_ERROR_THRESHOLD = st.number_input("WSE Error Threshold (ft)", 0.2)
+        st.write(
+            "Number of bins for the histogram to plot with respect to cells within the model"
+        )
+        NUM_BINS = st.number_input("Number of Bins", 100)
+
+    # Assets
+    report_file = os.path.join(
+        currDir, "assets", "FFRD-RAS-Report-Automated-Template.docx"
+    )
+    nid_parquet_file = os.path.join(currDir, "assets", "nid_inventory.parquet")
 
     # Create and set an event loop
     try:
@@ -73,73 +285,81 @@ if __name__ == "__main__":
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
-    if st.button("Run Report"):
-        if REPORT_FILE is not None:
-            report_document = Document(REPORT_FILE)
-            try:
-                main_auto_report(
-                    GEOM_HDF_PATH,
-                    PLAN_HDF_PATH,
-                    REPORT_FILE,
-                    DOMAIN_ID,
-                    STREAM_THRESHOLD,
-                    NLCD_RES,
-                    NLCD_YR,
-                    WSE_ERROR_THRESHOLD,
-                    NUM_BINS,
-                    rootDir,
-                    session_id
+    col4, col5, col6 = st.columns(3)
+
+    with col4:
+        st.subheader("Generate Report")
+        st.write("Click the button below to run the report.")
+        if st.button("Begin Report Generation"):
+            if GEOM_HDF_PATH is not None and PLAN_HDF_PATH is not None:
+                report_document = Document(report_file)
+                try:
+                    main_auto_report(
+                        GEOM_HDF_PATH,
+                        PLAN_HDF_PATH,
+                        report_document,
+                        DOMAIN_ID,
+                        STREAM_THRESHOLD,
+                        NLCD_RES,
+                        NLCD_YR,
+                        WSE_ERROR_THRESHOLD,
+                        NUM_BINS,
+                        nid_parquet_file,
+                        NID_DAM_HEIGHT,
+                        session_data_dir,
+                        active_streamlit=True,
+                    )
+                    st.session_state["data_acquired"] = True
+                    st.success("Report successfully generated!")
+                except Exception as e:
+                    st.error(f"Error: {e}")
+                    st.stop()
+            else:
+                st.error(
+                    "Please provide the required inputs before generating the report."
                 )
-                st.session_state["data_acquired"] = True
-                st.success("Report successfully generated!")
-            except Exception as e:
-                st.error(f"Error: {e}")
-                st.stop()
 
-    if st.session_state["data_acquired"]:
-        # Provide a download link to the generated report
-        st.subheader("Download Report")
+        if st.session_state["data_acquired"]:
+            # Provide a download link to the generated report
+            st.subheader("Download Report")
 
-        # give user option to download data
-        if st.session_state["request_zip"] is True:
-            request_zip_default = True
-        else:
-            request_zip_default = False
+            # give user option to download data
+            if st.session_state["request_zip"] is True:
+                request_zip_default = True
+            else:
+                request_zip_default = False
 
-        request_zip = st.checkbox(
-            "I want to download the data as a zip file", value=request_zip_default
-        )
-
-        if request_zip:
-            st.session_state["request_zip"] = True
-
-        if request_zip == True:
-
-            # write session data before creating zip
-            write_session_parameters(st.session_state)
-
-            # create the zip file if it doesn't exists
-            session_directory = st.session_state["session_data_dir"]
-            parent_dir = Path(session_directory).parent
-            zip_name = f"{Path(session_directory).name}.zip"
-            output_zip_file = os.path.join(parent_dir, zip_name)
-            st.session_state["session_zip"] = output_zip_file
-            print(f"zip file: {output_zip_file}")
-            session_zip = compress_directory(
-                st.session_state["session_data_dir"], output_zip_file
+            request_zip = st.checkbox(
+                "I want to download the data as a zip file", value=request_zip_default
             )
 
-            # for unknown reasons, might have to create zip (might just be a development aberation)
-            if not os.path.exists(session_zip):
-                with zipfile.ZipFile(session_zip, "w") as file:
-                    pass
+            if request_zip:
+                st.session_state["request_zip"] = True
 
-            with open(session_zip, "rb") as fp:
-                # with open(output_zip_file, "rb") as fp:
-                btn = st.download_button(
-                    label="Download ZIP file",
-                    data=fp,
-                    file_name=zip_name,
-                    mime="application/zip",
+            if request_zip == True:
+                # write session data before creating zip
+                write_session_parameters(st.session_state)
+
+                # create the zip file if it doesn't exists
+                parent_dir = Path(session_data_dir).parent
+                zip_name = f"{Path(session_data_dir).name}.zip"
+                output_zip_file = os.path.join(parent_dir, zip_name)
+                st.session_state["session_zip"] = output_zip_file
+                print(f"zip file: {output_zip_file}")
+                session_zip = compress_directory(
+                    st.session_state["session_data_dir"], output_zip_file
                 )
 
+                # for unknown reasons, might have to create zip (might just be a development aberation)
+                if not os.path.exists(session_zip):
+                    with zipfile.ZipFile(session_zip, "w") as file:
+                        pass
+
+                with open(session_zip, "rb") as fp:
+                    # with open(output_zip_file, "rb") as fp:
+                    btn = st.download_button(
+                        label="Download ZIP file",
+                        data=fp,
+                        file_name=zip_name,
+                        mime="application/zip",
+                    )
