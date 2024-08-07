e20225db64b86e864303c16a2ab1acb80450c623
zherbz <zherbz32@gmail.com>
2024-08-07 19:25:14 +0000

    replaced get_nid_dams() with new function that instead uses a static parquet file within the src/assets folder. This is due to the pygeohydro function failing to access the NID server even when the server is working.

--- src/auto_report/hy_river.py
+++ src/auto_report/hy_river.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 
 # Imports #####################################################################
+import requests
 import pandas as pd
+from shapely.geometry import Point
 import geopandas as gpd
 import py3dep
-from pygeohydro import NWIS, DataNotAvailableError
+from pygeohydro import (
+    NWIS,
+    DataNotAvailableError,
+    ServiceUnavailableError,
+    ServiceError,
+)
+from pygeoutils import EmptyResponseError
+import xml.etree.ElementTree as ET
 import pygeohydro as gh
 from pynhd import HP3D, WaterData, NHDPlusHR
 
 # Functions ###################################################################
+
 
 def get_dem_data(model_perimeter: gpd.GeoDataFrame):
     """
@@ -81,28 +91,24 @@ def get_nhd_flowlines(model_perimeter: gpd.GeoDataFrame):
     except Exception as e:
         print(f"Failed retrieving flowlines from NHD")
         print("Attempting to retrieve flowlines from the local NHDPlus HR dataset...")
-        try:
-            # Create an instance of the NHDPlus HR class
-            nhd_hr = NHDPlusHR("flowline")
-            # Query the NHDPlus HR flowlines that intersect the model perimeter
-            flowlines = nhd_hr.bygeom(
-                model_perimeter.geometry.iloc[0], model_perimeter.crs
-            )
-            return flowlines
-        except Exception as e:
-            print(f"Failed retrieving flowlines from NHDPlus HR:")
-            print("Attempting to retrieve flowlines from 3DHP...")
-            try:
-                # Create an instance of the 3DHP class
-                hp3d = HP3D("flowline")
-                # Query the 3DHP flowlines that intersect the model perimeter
-                flowlines = hp3d.bygeom(
-                    model_perimeter.geometry.iloc[0], model_perimeter.crs
-                )
-                return flowlines
-            except Exception as e:
-                print(f"Failed retrieving flowlines from 3DHP: {e}")
-                return None
+    try:
+        # Create an instance of the NHDPlus HR class
+        nhd_hr = NHDPlusHR("flowline")
+        # Query the NHDPlus HR flowlines that intersect the model perimeter
+        flowlines = nhd_hr.bygeom(model_perimeter.geometry.iloc[0], model_perimeter.crs)
+        return flowlines
+    except Exception as e:
+        print(f"Failed retrieving flowlines from NHDPlus HR:")
+        print("Attempting to retrieve flowlines from 3DHP...")
+    try:
+        # Create an instance of the 3DHP class
+        hp3d = HP3D("flowline")
+        # Query the 3DHP flowlines that intersect the model perimeter
+        flowlines = hp3d.bygeom(model_perimeter.geometry.iloc[0], model_perimeter.crs)
+        return flowlines
+    except Exception as e:
+        return_statement = f"Data Unavailable: {e}"
+        return return_statement
 
 
 def get_nid_dams(model_perimeter: gpd.GeoDataFrame):
@@ -119,15 +125,89 @@ def get_nid_dams(model_perimeter: gpd.GeoDataFrame):
     gpd.GeoDataFrame
         The NID dams within the model perimeter
     """
-    try:
-        # Create an instance of the NID class
-        nid = gh.NID()
-        # Query the NID dams that intersect the model perimeter
-        dams = nid.get_bygeom(model_perimeter.geometry.iloc[0], model_perimeter.crs)
-        return dams
-    except Exception as e:
-        print(f"Failed retrieving dams from NID: {e}")
+    # First check if the NID server is available
+    url = "https://nid.sec.usace.army.mil/api/nation/gpkg"
+    response = requests.get(url)
+    response.status_code
+    if response.status_code != 200:
+        print(
+            f"NID server is currently unavailable. Server Code: {response.status_code}"
+        )
         return None
+    else:
+        print(f"NID server is available. Server Code: {response.status_code}")
+        try:
+            # Create an instance of the NID class
+            nid = gh.NID()
+            # Query the NID dams that intersect the model perimeter
+            dams = nid.get_bygeom(model_perimeter.geometry.iloc[0], model_perimeter.crs)
+            return dams
+        except ServiceError as e:
+            print(
+                f"Failed retrieving dams from NID. The server is currently unavailable: {e}"
+            )
+            return None
+        except EmptyResponseError as e:
+            print(f"No dams found within the model perimeter: {e}")
+            return None
+        except Exception as e:
+            print(f"Failed retrieving dams from NID: {e}")
+            return None
+
+
+def filter_nid(
+    parquet_file_path: str,
+    model_perimeter: gpd.GeoDataFrame,
+    height_threshold: int = 50,
+):
+    """
+    Filter the National Inventory of Dams (NID) data to only include points
+    within the specified state and above the specified height threshold.
+
+    Parameters
+    ----------
+    parquet_file_path : str
+        The path to the Parquet file containing the NID data.
+    model_perimeter : gpd.GeoDataFrame
+        The GeoDataFrame representing the model perimeter.
+    height_threshold : int
+        The vertical dam height threshold to filter the NID data by.
+
+    Returns
+    -------
+    filtered_nid_gdf : gpd.GeoDataFrame
+        A GeoDataFrame containing the filtered NID data.
+    """
+
+    # Read the Parquet file containing point data
+    nid_df = pd.read_parquet(parquet_file_path)
+    # Columns in the Parquet file: ['name', 'latitude', 'longitude', 'nidId', 'damHeight']
+
+    # Filter to the points with a height greater or equal to the threshold
+    nid_df = nid_df[nid_df["damHeight"] >= height_threshold]
+
+    # Ensure the point data has 'latitude' and 'longitude' columns
+    if "latitude" not in nid_df.columns or "longitude" not in nid_df.columns:
+        raise ValueError(
+            "The Parquet file must contain 'latitude' and 'longitude' columns."
+        )
+
+    # Filter to only the latitude and longitude columns
+    nid_df = nid_df[["latitude", "longitude"]]
+
+    if len(nid_df) == 0:
+        return None
+    else:
+        # Convert the point data to a GeoDataFrame
+        nid_gdf = gpd.GeoDataFrame(
+            nid_df,
+            geometry=[Point(xy) for xy in zip(nid_df.longitude, nid_df.latitude)],
+            crs="EPSG:4326",  # Assuming the coordinates are in WGS84
+        )
+        # Perform a spatial join to filter points within the polygon
+        filtered_nid_gdf = gpd.sjoin(nid_gdf, model_perimeter, predicate="within")
+
+        return filtered_nid_gdf
 
 
 def get_nlcd_data(model_perimeter: gpd.GeoDataFrame, resolution: int, year: int):
@@ -148,16 +228,36 @@ def get_nlcd_data(model_perimeter: gpd.GeoDataFrame, resolution: int, year: int)
     gpd.GeoDataFrame
         The NLCD data within the model perimeter
     """
-    try:
-        # Retrieve the NLCD data at the specified resolution and year
-        nlcd = gh.nlcd_bygeom(model_perimeter, resolution, years={"cover": year})[0]
-        return nlcd
-    except Exception as e:
-        print(f"Failed retrieving NLCD data: {e}")
-        return None
+    # First test if the NLCD server is available
+    url = "https://www.mrlc.gov/geoserver/mrlc_display/NLCD_2001_2021_Land_Cover_Change_Index_L48/wms?"
+    response = requests.get(url)
 
-def get_usgs_stations(model_perimeter: gpd.GeoDataFrame,
-                      dates: tuple):
+    if response.status_code != 200:
+        return_statement = (
+            f"NLCD server is currently unavailable. Server Code: {response.status_code}"
+        )
+        return return_statement
+    else:
+        print(f"NLCD server is available. Server Code: {response.status_code}")
+        try:
+            # Retrieve the NLCD data at the specified resolution and year
+            nlcd = gh.nlcd_bygeom(model_perimeter, resolution, years={"cover": year})[0]
+            return nlcd
+        except ServiceUnavailableError as e:
+            return_statement = f"Failed retrieving NLCD data. {e}"
+            return return_statement
+        except ServiceError as e:
+            return_statement = f"Failed retrieving NLCD data. {e}"
+            return return_statement
+        except DataNotAvailableError as e:
+            return_statement = f"Failed retrieving NLCD data. {e}"
+            return return_statement
+        except ET.ParseError as e:
+            return_statement = f"Failed retrieving NLCD data. {e}"
+            return return_statement
+
+
+def get_usgs_stations(model_perimeter: gpd.GeoDataFrame, dates: tuple):
     """
     Get the USGS gage stations within the model perimeter
 
@@ -177,7 +277,7 @@ def get_usgs_stations(model_perimeter: gpd.GeoDataFrame,
     nwis = NWIS()
 
     # Get the bounding box of the model perimeter
-    bbox = tuple(model_perimeter.bounds.values[0]) # (minx, miny, maxx, maxy)
+    bbox = tuple(model_perimeter.bounds.values[0])  # (minx, miny, maxx, maxy)
 
     # Query gage stations with daily values
     query_dv = {
@@ -208,13 +308,18 @@ def get_usgs_stations(model_perimeter: gpd.GeoDataFrame,
     # Convert df_gages_usgs to a geodataframe
     df_gages_usgs = gpd.GeoDataFrame(
         df_gages_usgs,
-        geometry=gpd.points_from_xy(df_gages_usgs.dec_long_va, df_gages_usgs.dec_lat_va),
+        geometry=gpd.points_from_xy(
+            df_gages_usgs.dec_long_va, df_gages_usgs.dec_lat_va
+        ),
         crs="EPSG:4326",
     )
     # Filter the gages to only include those within the model perimeter
     # This is necesarry since the NWIS() class only support query by rectangular bbox
-    df_gages_usgs = df_gages_usgs[df_gages_usgs.within(model_perimeter.geometry.iloc[0])]
+    df_gages_usgs = df_gages_usgs[
+        df_gages_usgs.within(model_perimeter.geometry.iloc[0])
+    ]
     return df_gages_usgs
+
 
 def get_nwis_streamflow(df_gages_usgs: gpd.GeoDataFrame, dates: tuple):
     """
@@ -250,4 +355,7 @@ def get_nwis_streamflow(df_gages_usgs: gpd.GeoDataFrame, dates: tuple):
         )
         return qobs_ds
     except DataNotAvailableError as e:
-        raise ValueError(f"Failed to get daily values for the gage stations: {e}")
+        return_statement = (
+            f"Failed to get daily and instantaneous values for the gage stations: {e}"
+        )
+        return return_statement
