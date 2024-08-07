f1cb36151f4542fbc754f58617275084d38818fc
zherbz <zherbz32@gmail.com>
2024-08-07 19:25:39 +0000

    updated script for testing

--- tests/report_runner.py
+++ tests/report_runner.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from docx import Document
 
 # Determine where the script is located
 currDir = os.path.dirname(os.path.realpath(__file__))
@@ -16,16 +17,16 @@ if __name__ == "__main__":
     # Example paths to GEOM and PLAN HDF files for the Trinity Pilot Study
 
     # EaFT Lavon
-    GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.g06.hdf"
-    PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.p01.hdf"
+    # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.g06.hdf"
+    # PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.p01.hdf"
 
     # R-Chambers Creek
     # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/R-ChambersCr/R-ChambersCr/Trinity_1203_R_Ch_Cr.g01.hdf"
     # PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/R-ChambersCr/R-ChambersCr/Trinity_1203_R_Ch_Cr.p01.hdf"
 
     # Denton
-    # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.g01.hdf"
-    # PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.p03.hdf"
+    GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.g01.hdf"
+    PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.p03.hdf"
 
     # C Mill Creek
     # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/C-MillCrk/Model/C_MillCrk_1203_Upper_New/C_MillCrk_1203_Upper_New.g02.hdf"
@@ -41,17 +42,28 @@ if __name__ == "__main__":
     NLCD_YR = 2019  # 2019 NLCD data
     WSE_ERROR_THRESHOLD = 0.2  # Threshold for the histogram's x-axis
     NUM_BINS = 100  # Number of bins for the histogram
+    NID_DAM_HEIGHT = 50  # Dam height threshold for the NID inventory
     ####### End User Input #######
+
+    REPORT_PATH = r"/workspaces/ffrd-auto-reports/src/assets/FFRD-RAS-Report-Automated-Template.docx"
+    report_document = Document(REPORT_PATH)
+    NID_PARQUET_PATH = r"/workspaces/ffrd-auto-reports/src/assets/nid_inventory.parquet"
+    os.makedirs(r"/workspaces/ffrd-auto-reports/session/test", exist_ok=True)
+    OUTPUT_PATH = r"/workspaces/ffrd-auto-reports/session/test"
 
     # Main function to run the auto report
     main_auto_report(
         GEOM_HDF_PATH,
         PLAN_HDF_PATH,
+        report_document,
         DOMAIN_ID,
         STREAM_THRESHOLD,
         NLCD_RES,
         NLCD_YR,
         WSE_ERROR_THRESHOLD,
         NUM_BINS,
-        rootDir,
+        NID_PARQUET_PATH,
+        NID_DAM_HEIGHT,
+        OUTPUT_PATH,
+        active_streamlit=False,
     )
