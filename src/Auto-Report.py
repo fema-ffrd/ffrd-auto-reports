73c497cef1c8b038b292bfc31569466c77bee9ce
zherbz <zherbz32@gmail.com>
2024-08-08 13:18:12 +0000

    changed widget for NLCD year to a select box for only 2019 and 2021

--- src/Auto-Report.py
+++ src/Auto-Report.py
@@ -18,6 +18,7 @@ from app_utilities import (
     compress_directory,
     write_session_parameters,
     initialize_session,
+    particles_js,
 )
 
 # Determine where the script is located
@@ -32,154 +33,9 @@ sys.path.append(srcDir)
 
 from auto_report import main_auto_report
 
-particles_js = """<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Particles.js</title>
-  <style>
-  #particles-js {
-    position: fixed;
-    width: 100vw;
-    height: 100vh;
-    top: 0;
-    left: 0;
-    z-index: -1; /* Send the animation to the back */
-  }
-  .content {
-    position: relative;
-    z-index: 1;
-    color: white;
-  }
-  
-</style>
-</head>
-<body>
-  <div id="particles-js"></div>
-  <div class="content">
-    <!-- Placeholder for Streamlit content -->
-  </div>
-  <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
-  <script>
-    particlesJS("particles-js", {
-      "particles": {
-        "number": {
-          "value": 500,
-          "density": {
-            "enable": true,
-            "value_area": 800
-          }
-        },
-        "color": {
-          "value": "#0000ff"
-        },
-        "shape": {
-          "type": "circle",
-          "stroke": {
-            "width": 0,
-            "color": "#ffffff"
-          },
-          "polygon": {
-            "nb_sides": 5
-          },
-          "image": {
-            "src": "img/github.svg",
-            "width": 100,
-            "height": 100
-          }
-        },
-        "opacity": {
-          "value": 0.8,
-          "random": false,
-          "anim": {
-            "enable": false,
-            "speed": 1,
-            "opacity_min": 0.2,
-            "sync": false
-          }
-        },
-        "size": {
-          "value": 2,
-          "random": true,
-          "anim": {
-            "enable": false,
-            "speed": 50,
-            "size_min": 0.1,
-            "sync": false
-          }
-        },
-        "line_linked": {
-          "enable": true,
-          "distance": 100,
-          "color": "#0000ff",
-          "opacity": 0.5,
-          "width": 1
-        },
-        "move": {
-          "enable": true,
-          "speed": 0.2,
-          "direction": "none",
-          "random": false,
-          "straight": false,
-          "out_mode": "out",
-          "bounce": true,
-          "attract": {
-            "enable": false,
-            "rotateX": 600,
-            "rotateY": 1200
-          }
-        }
-      },
-      "interactivity": {
-        "detect_on": "canvas",
-        "events": {
-          "onhover": {
-            "enable": true,
-            "mode": "grab"
-          },
-          "onclick": {
-            "enable": true,
-            "mode": "repulse"
-          },
-          "resize": true
-        },
-        "modes": {
-          "grab": {
-            "distance": 100,
-            "line_linked": {
-              "opacity": 1
-            }
-          },
-          "bubble": {
-            "distance": 400,
-            "size": 2,
-            "duration": 2,
-            "opacity": 0.5,
-            "speed": 1
-          },
-          "repulse": {
-            "distance": 200,
-            "duration": 0.4
-          },
-          "push": {
-            "particles_nb": 2
-          },
-          "remove": {
-            "particles_nb": 3
-          }
-        }
-      },
-      "retina_detect": true
-    });
-  </script>
-</body>
-</html>
-"""
-
 # Main Page ###################################################################
 
-st.set_page_config(layout="wide", page_icon=":globe_with_meridians:")
+st.set_page_config(layout="wide", page_icon="💧")
 
 if __name__ == "__main__":
     # setup a session data when app is first opened or browser reset
@@ -262,7 +118,7 @@ if __name__ == "__main__":
         st.write("Spatial resolution of the NLCD data")
         NLCD_RES = st.number_input("NLCD Resolution", 30)
         st.write("National Land Cover Database year of coverage")
-        NLCD_YR = st.number_input("NLCD Year", 2019)
+        NLCD_YR = st.selectbox("NLCD Year", ["2019", "2021"])
         st.write(
             "Threshold of the histogram's x-axis for plotting the water surface elevation errors"
         )
