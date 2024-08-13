2582e6654a3536b6fa3f07c8e49016b9ffe6aeb3
zherbz <zherbz32@gmail.com>
2024-08-13 00:38:36 +0000

    minor updates

--- tests/report_runner.py
+++ tests/report_runner.py
@@ -16,6 +16,10 @@ if __name__ == "__main__":
     ####### Begin User Input #######
     # Example paths to GEOM and PLAN HDF files for the Trinity Pilot Study
 
+    # Denton
+    # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.g01.hdf"
+    # PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.p03.hdf"
+
     # EaFT Lavon
     # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.g06.hdf"
     # PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.p01.hdf"
@@ -24,13 +28,9 @@ if __name__ == "__main__":
     # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/R-ChambersCr/R-ChambersCr/Trinity_1203_R_Ch_Cr.g01.hdf"
     # PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/R-ChambersCr/R-ChambersCr/Trinity_1203_R_Ch_Cr.p01.hdf"
 
-    # Denton
-    GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.g01.hdf"
-    PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.p03.hdf"
-
     # C Mill Creek
-    # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/C-MillCrk/Model/C_MillCrk_1203_Upper_New/C_MillCrk_1203_Upper_New.g02.hdf"
-    # PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/C-MillCrk/Model/C_MillCrk_1203_Upper_New/C_MillCrk_1203_Upper_New.p03.hdf"
+    GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/C-MillCrk/Model/C_MillCrk_1203_Upper_New/C_MillCrk_1203_Upper_New.g02.hdf"
+    PLAN_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/C-MillCrk/Model/C_MillCrk_1203_Upper_New/C_MillCrk_1203_Upper_New.p03.hdf"
 
     # LT Below Richland
     # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/LT-BelowRichlandChambers/Model/LT-BelowRichlandCha.g02.hdf"
@@ -42,11 +42,10 @@ if __name__ == "__main__":
     NLCD_YR = 2019  # 2019 NLCD data
     WSE_ERROR_THRESHOLD = 0.2  # Threshold for the histogram's x-axis
     NUM_BINS = 100  # Number of bins for the histogram
-    NID_DAM_HEIGHT = 50  # Dam height threshold for the NID inventory
+    NID_DAM_HEIGHT = 20  # Dam height threshold for the NID inventory
     ####### End User Input #######
 
     REPORT_PATH = r"/workspaces/ffrd-auto-reports/src/assets/FFRD-RAS-Report-Automated-Template.docx"
-    report_document = Document(REPORT_PATH)
     NID_PARQUET_PATH = r"/workspaces/ffrd-auto-reports/src/assets/nid_inventory.parquet"
     os.makedirs(r"/workspaces/ffrd-auto-reports/session/test", exist_ok=True)
     OUTPUT_PATH = r"/workspaces/ffrd-auto-reports/session/test"
@@ -55,7 +54,7 @@ if __name__ == "__main__":
     main_auto_report(
         GEOM_HDF_PATH,
         PLAN_HDF_PATH,
-        report_document,
+        REPORT_PATH,
         DOMAIN_ID,
         STREAM_THRESHOLD,
         NLCD_RES,
