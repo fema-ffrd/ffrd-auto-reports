6d617224e81b3f22d2f18f050899fd3f2711d20c
zherbz <zherbz32@gmail.com>
2024-08-26 21:24:18 +0000

    updated fill_computation_settings_table to update as the loop progresses through each plan. black formatting

--- src/auto_report/tables.py
+++ src/auto_report/tables.py
@@ -58,7 +58,6 @@ def fill_gage_table(report_keywords: dict, df_gages_usgs: pd.DataFrame):
     nwis = NWIS()
     for idx, row in df_gages_usgs.iterrows():
         station_id = row["site_no"]  # 08059590
-        station = f"USGS-{station_id}"  # USGS-08059590
         station_name = row["station_nm"]  # Willow Creek at Highway 80
         begin_date = row["begin_date"].strftime("%Y-%m-%d")
         end_date = row["end_date"].strftime("%Y-%m-%d")
@@ -77,10 +76,13 @@ def fill_gage_table(report_keywords: dict, df_gages_usgs: pd.DataFrame):
 
     return report_keywords
 
-def fill_nid_dams_table(report_keywords: dict,
-                        nid_parquet_file_path: str,
-                        model_perimeter: gpd.GeoDataFrame,
-                        nid_dam_height: int):
+
+def fill_nid_dams_table(
+    report_keywords: dict,
+    nid_parquet_file_path: str,
+    model_perimeter: gpd.GeoDataFrame,
+    nid_dam_height: int,
+):
     """
     Update the report_keywords dictionary with the values from the NID dams within the model perimeter.
 
@@ -110,11 +112,12 @@ def fill_nid_dams_table(report_keywords: dict,
         report_keywords[f"table06_dam0{idx+1}_id"] = row["nidId"]
         report_keywords[f"table06_dam0{idx+1}_name"] = row["name"]
         report_keywords[f"table06_dam0{idx+1}_height"] = str(row["damHeight"])
-        
+
     return report_keywords
 
+
 def fill_computation_settings_table(
-    report_keywords: dict, plan_params: dict, plan_attrs: dict
+    report_keywords: dict, plan_params: dict, plan_attrs: dict, plan_idx: int
 ):
     """
     Update the report_keywords dictionary with the values from the plan_params and plan_attrs dictionaries.
@@ -127,6 +130,8 @@ def fill_computation_settings_table(
         Dictionary containing the plan parameters.
     plan_attrs : dict
         Dictionary containing the plan attributes.
+    plan_idx : int
+        Index of the plan. One of [1, 2, 3, 4, 5, 6].
 
     Returns
     -------
@@ -135,20 +140,21 @@ def fill_computation_settings_table(
     """
 
     # Update Table 9: 2D Comutational Solver Tolerances and Settings
-    report_keywords["table09_iwf"] = str(plan_params["2D Theta"])
-    report_keywords["table09_wst"] = str(plan_params["2D Water Surface Tolerance"])
-    report_keywords["table09_volt"] = str(plan_params["2D Volume Tolerance"])
-    report_keywords["table09_max_iter"] = str(plan_params["2D Maximum Iterations"])
-    report_keywords["table09_fts"] = plan_attrs["Computation Time Step Base"]
-    report_keywords["table09_eqn"] = plan_params["2D Equation Set"]
-    report_keywords["table09_output_interval"] = plan_attrs["Base Output Interval"]
+    report_keywords[f"plan0{plan_idx}_iwf"] = str(round(plan_params["2D Theta"],2))
+    report_keywords[f"plan0{plan_idx}_wst"] = str(round(plan_params["2D Water Surface Tolerance"],2))
+    report_keywords[f"plan0{plan_idx}_volt"] = str(round(plan_params["2D Volume Tolerance"],2))
+    report_keywords[f"plan0{plan_idx}_max_iter"] = str(int(plan_params["2D Maximum Iterations"]))
+    report_keywords[f"plan0{plan_idx}_fts"] = plan_attrs["Computation Time Step Base"]
+    report_keywords[f"plan0{plan_idx}_eqn"] = plan_params["2D Equation Set"]
+    report_keywords[f"plan0{plan_idx}_output_interval"] = plan_attrs["Base Output Interval"]
 
     return report_keywords
+
 
 def evaluate_metrics(x):
     """
     Evaluate the calibration metrics according to the USACE guidelines
-    
+
     Parameters
     ----------
     x : pd.DataFrame
@@ -168,11 +174,7 @@ def evaluate_metrics(x):
             else (
                 "Good"
                 if x > 0.55 and x <= 0.65
-                else (
-                    "Satisfactory"
-                    if x > 0.4 and x <= 0.55
-                    else "Unsatisfactory"
-                )
+                else ("Satisfactory" if x > 0.4 and x <= 0.55 else "Unsatisfactory")
             )
         )
     )
@@ -183,11 +185,7 @@ def evaluate_metrics(x):
             else (
                 "Good"
                 if x > 0.55 and x <= 0.65
-                else (
-                    "Satifactory"
-                    if x > 0.4 and x <= 0.55
-                    else "Unsatisfactory"
-                )
+                else ("Satifactory" if x > 0.4 and x <= 0.55 else "Unsatisfactory")
             )
         )
     )
@@ -198,11 +196,7 @@ def evaluate_metrics(x):
             else (
                 "Good"
                 if x > 0.6 and x <= 0.7
-                else (
-                    "Satisfactory"
-                    if x > 0.7 and x <= 0.8
-                    else "Unsatisfactory"
-                )
+                else ("Satisfactory" if x > 0.7 and x <= 0.8 else "Unsatisfactory")
             )
         )
     )
@@ -213,17 +207,16 @@ def evaluate_metrics(x):
             else (
                 "Good"
                 if x >= 15 and x < 20
-                else (
-                    "Satifactory"
-                    if x >= 20 and x < 30
-                    else "Unsatisfactory"
-                )
+                else ("Satifactory" if x >= 20 and x < 30 else "Unsatisfactory")
             )
         )
     )
     return df
 
-def fill_calibration_metrics_table(report_keywords: dict, plan_index: int, metrics_df: pd.DataFrame):
+
+def fill_calibration_metrics_table(
+    report_keywords: dict, plan_index: int, metrics_df: pd.DataFrame, parameter: str
+):
     """
     Update the report_keywords dictionary with the values from the metrics_df DataFrame.
 
@@ -235,7 +228,9 @@ def fill_calibration_metrics_table(report_keywords: dict, plan_index: int, metri
         Index of the plan. One of [1, 2, 3, 4, 5, 6].
     metrics_df : pd.DataFrame
         DataFrame containing the calibration metrics. Columns include NSE, RSR, PBIAS, and R2. Indeces are the gage IDs.
-
+    parameter : str
+        Parameter to be evaluated. One of 'Flow' or 'Stage'.
+        
     Returns
     -------
     report_keywords : dict
@@ -243,21 +238,33 @@ def fill_calibration_metrics_table(report_keywords: dict, plan_index: int, metri
     """
     row_index = 1
     metrics_df = metrics_df.round(2)
+    parameter = parameter.lower()
     for gage_id, row in metrics_df.iterrows():
         report_keywords[f"plan0{plan_index}_gage0{row_index}"] = gage_id
-        report_keywords[f"plan0{plan_index}_gage0{row_index}_flow_nse"] = str(row["NSE"])
-        report_keywords[f"plan0{plan_index}_gage0{row_index}_flow_rsr"] = str(row["RSR"])
-        report_keywords[f"plan0{plan_index}_gage0{row_index}_flow_pbias"] = str(row["PBIAS"])
-        report_keywords[f"plan0{plan_index}_gage0{row_index}_flow_r2"] = str(row["R2"])
+        report_keywords[f"plan0{plan_index}_gage0{row_index}_{parameter}_nse"] = str(
+            row["NSE"]
+        )
+        report_keywords[f"plan0{plan_index}_gage0{row_index}_{parameter}_rsr"] = str(
+            row["RSR"]
+        )
+        report_keywords[f"plan0{plan_index}_gage0{row_index}_{parameter}_pbias"] = str(
+            row["PBIAS"]
+        )
+        report_keywords[f"plan0{plan_index}_gage0{row_index}_{parameter}_r2"] = str(row["R2"])
         row_index = row_index + 1
 
     row_index = 1
     eval_df = evaluate_metrics(metrics_df)
     for gage_id, row in eval_df.iterrows():
-        report_keywords[f"plan0{plan_index}_gage0{row_index}_flow_nse_eval"] = row["NSE"]
-        report_keywords[f"plan0{plan_index}_gage0{row_index}_flow_rsr_eval"] = row["RSR"]
-        report_keywords[f"plan0{plan_index}_gage0{row_index}_flow_pbias_eval"] = row["PBIAS"]
-        report_keywords[f"plan0{plan_index}_gage0{row_index}_flow_r2_eval"] = row["R2"]
+        report_keywords[f"plan0{plan_index}_gage0{row_index}_{parameter}_nse_eval"] = row[
+            "NSE"
+        ]
+        report_keywords[f"plan0{plan_index}_gage0{row_index}_{parameter}_rsr_eval"] = row[
+            "RSR"
+        ]
+        report_keywords[f"plan0{plan_index}_gage0{row_index}_{parameter}_pbias_eval"] = row[
+            "PBIAS"
+        ]
+        report_keywords[f"plan0{plan_index}_gage0{row_index}_{parameter}_r2_eval"] = row["R2"]
         row_index = row_index + 1
     return report_keywords
-
