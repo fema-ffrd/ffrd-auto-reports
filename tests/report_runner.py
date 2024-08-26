0aa22ab0c0684c4d0bf7daa6856b4a5662fc3dec
zherbz <zherbz32@gmail.com>
2024-08-26 21:25:12 +0000

    black formatting

--- tests/report_runner.py
+++ tests/report_runner.py
@@ -17,12 +17,12 @@ if __name__ == "__main__":
 
     # Denton
     GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Trinity_1203_Denton.g01.hdf"
-    PLAN_HDF_FILES = [".p01.hdf", ".p02.hdf", ".p03.hdf", ".p04.hdf"]
+    PLAN_HDF_FILES = [".p01.hdf", ".p02.hdf", ".p03.hdf", ".p04.hdf", ".p06.hdf", ".p11.hdf"]
     NLCD_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/Denton/Trinity_1203_Denton/Reference/LandCover/Denton_LandCover.tif"
 
     # EaFT Lavon
     # GEOM_HDF_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/EaFT_Lavon.g06.hdf"
-    # PLAN_HDF_FILES = [".p01.hdf", ".p02.hdf", ".p05.hdf", ".p06.hdf"]
+    # PLAN_HDF_FILES = [".p01.hdf", ".p02.hdf", ".p05.hdf", ".p06.hdf", ".p08.hdf", ".p10.hdf"]
     # NLCD_PATH = "s3://trinity-pilot/Checkpoint1-ModelsForReview/Hydraulics/EaFT-Lavon/Model/Land Classification/NLCD_2021_Trinity_ClipLavon.tif"
 
     # R-Chambers Creek
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     WSE_ERROR_THRESHOLD = 0.2  # Threshold for the histogram's x-axis
     NUM_BINS = 100  # Number of bins for the histogram
     NID_DAM_HEIGHT = 20  # Dam height threshold for the NID inventory
-    GAGE_COLLECTION_METHOD = "Only collect gages that provide current data" # or "Collect all gages, old and current"
+    GAGE_COLLECTION_METHOD = "Only collect gages that provide current data"  # or "Collect all gages, old and current"
     ####### End User Input #######
 
     REPORT_PATH = r"/workspaces/ffrd-auto-reports/src/assets/FFRD-RAS-Report-Automated-Template.docx"
